4ecbbc6b27b8ec825b3d41be3a7d5dac8295f828
abernardo <abernardo>
2025-07-18 01:32:00 -0300

    tests passing

--- ej2.cpp
+++ ej2.cpp
@@ -6,7 +6,7 @@ void ImprimeVector(double Vector[], int NumElementos)
     for (int i = 0; i < NumElementos; ++i)
     {
         std::cout << Vector[i];
-        if (i < NumElementos + 1)
+        if (i < NumElementos - 1)
             std::cout << " ";
     }
     std::cout << std::endl;
